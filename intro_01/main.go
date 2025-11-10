86f232ae79fc0d4b15dedcca560b6c95f73e5611
Naveen.P <naveen.pillalamarri@rapido.bike>
2025-11-10 07:43:55 +0530

    if-else

--- intro_01/main.go
+++ intro_01/main.go
@@ -6,4 +6,21 @@ import "github.com/fatih/color"
 func main() {
 	fmt.Println("Hello, World")
     color.Red("Hello, World")
+
+    //strings
+    text := `open
+    \n
+    close
+    `
+    fmt.Println(text)
+    isAdult(18)
+}
+
+
+func isAdult (age int) {
+    if age >= 18 {
+        fmt.Println("Adult")
+    }else{
+        fmt.Println("Teen")
+    }
 }
